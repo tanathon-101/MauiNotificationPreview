84aa268abd0c1f5e611385f837f94bfa7def4c94
tanathon-101 <tanaton.akko@gmail.com>
2025-06-08 16:57:57 +0700

    move file main

---
+++ MauiNotificationPreview/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+// File: Views/MainPage.xaml.cs
+using Microsoft.Maui.Controls;
+
+namespace MauiNotificationPreview.Views;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
